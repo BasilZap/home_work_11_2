e53c5ae115d1b39a306bc73f4f8b367616ea0a42
basil <basil@basil.ru>
2023-05-09 19:37:06 +0300

    set_text_title() added and docstrings added to project

--- func.py
+++ func.py
@@ -1,2 +1,16 @@
 def set_text_upper(some_text):
+    """
+    Функция делает заглавными все
+    буквы во всех словах
+    """
     return str(some_text).upper()
+
+
+def set_text_title(some_text):
+    """
+    Функция делает заглавными все
+    первые буквы каждого слова
+    :param some_text: str
+    :return: str
+    """
+    return str(some_text).title()
