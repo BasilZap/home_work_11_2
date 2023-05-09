748a117a85238461446f17bfee0f4aeca60f5103
basil <basil@basil.ru>
2023-05-09 20:22:32 +0300

    set_text_title() in func bug fixed

--- func.py
+++ func.py
@@ -6,11 +6,11 @@ def set_text_upper(some_text):
     return str(some_text).upper()
 
 
-def set_text_title(some_text):
+def set_text_title(some_text1):
     """
     Функция делает заглавными все
     первые буквы каждого слова
     :param some_text: str
     :return: str
     """
-    return str(some_text).title()
+    return str(some_text1).title()
